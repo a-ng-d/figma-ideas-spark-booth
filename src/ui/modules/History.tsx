2c330d7746201b53e6312e27182b7a0deff68ad4
Aurélien Grimaud <a.grimaud@involt.io>
2024-10-05 01:29:36 +0200

    refactor: adjust items style and add menu for filtering ideas

--- src/ui/modules/History.tsx
+++ src/ui/modules/History.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Dropdown,
   DropdownOption,
+  Menu,
   SimpleItem,
   layouts,
   texts,
@@ -44,37 +45,12 @@ export default class History extends React.Component<
         (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
       ),
       sortedBy: 'MOST_RECENT',
-      filteredBy: 'ALL',
+      filteredBy: 'NONE',
     }
   }
 
   // Handlers
-  typesHandler = () => {
-    const all: DropdownOption = {
-      label: 'All',
-      value: 'ALL',
-      feature: 'FILTER_BY_TYPE',
-      position: 0,
-      type: 'OPTION',
-      isActive: true,
-      isBlocked: false,
-      isNew: false,
-      children: [],
-      action: () =>
-        this.setState({
-          filteredBy: 'ALL',
-          ideas:
-            (() => {
-              if (this.state.sortedBy === 'MOST_RECENT') {
-                return this.onSortMostRecent(this.props.ideas)
-              }
-              if (this.state.sortedBy === 'OLDEST') {
-                return this.onSortMostRecent(this.props.ideas)
-              }
-              return []
-            })() || [],
-        }),
-    }
+  typesHandler = (): Array<DropdownOption> => {
     const types = Object.entries(
       this.props.ideas.reduce(
         (acc: { [key: string]: IdeaConfiguration[] }, idea) => {
@@ -124,7 +100,125 @@ export default class History extends React.Component<
             }),
         } as DropdownOption
       })
-    return [all, ...types]
+
+    const participants = Object.entries(
+      this.props.ideas.reduce(
+        (acc: { [key: string]: IdeaConfiguration[] }, idea) => {
+          const { userIdentity } = idea
+          if (!acc[userIdentity.fullName]) {
+            acc[userIdentity.fullName] = []
+          }
+          acc[userIdentity.fullName].push(idea)
+          return acc
+        },
+        {} as { [key: string]: IdeaConfiguration[] }
+      )
+    )
+      .sort((a, b) =>
+        a[1][0].userIdentity.fullName.localeCompare(
+          b[1][0].userIdentity.fullName
+        )
+      )
+      .map((entries) => {
+        return {
+          label: entries[1][0].userIdentity.fullName,
+          value: entries[1][0].userIdentity.id,
+          feature: 'FILTER_BY_PARTICIPANT',
+          position: 0,
+          type: 'OPTION',
+          isActive: true,
+          isBlocked: false,
+          isNew: false,
+          children: [],
+          action: () =>
+            this.setState({
+              filteredBy: entries[1][0].userIdentity.id,
+              ideas:
+                (() => {
+                  if (this.state.sortedBy === 'MOST_RECENT') {
+                    return this.onSortMostRecent(
+                      this.props.ideas.filter(
+                        (idea) =>
+                          idea.userIdentity.id === entries[1][0].userIdentity.id
+                      )
+                    )
+                  }
+                  if (this.state.sortedBy === 'OLDEST') {
+                    return this.onSortMostRecent(
+                      this.props.ideas.filter(
+                        (idea) =>
+                          idea.userIdentity.id === entries[1][0].userIdentity.id
+                      )
+                    )
+                  }
+                  return []
+                })() || [],
+            }),
+        } as DropdownOption
+      })
+    return [
+      {
+        label: 'None',
+        value: 'NONE',
+        feature: null,
+        position: 0,
+        type: 'OPTION',
+        isActive: true,
+        isBlocked: false,
+        isNew: false,
+        children: [],
+        action: () =>
+          this.setState({
+            filteredBy: 'NONE',
+            ideas:
+              (() => {
+                if (this.state.sortedBy === 'MOST_RECENT') {
+                  return this.onSortMostRecent(this.props.ideas)
+                }
+                if (this.state.sortedBy === 'OLDEST') {
+                  return this.onSortMostRecent(this.props.ideas)
+                }
+                return []
+              })() || [],
+          }),
+      },
+      {
+        label: null,
+        value: null,
+        feature: null,
+        position: 0,
+        type: 'SEPARATOR',
+        isActive: true,
+        isBlocked: false,
+        isNew: false,
+        children: [],
+        action: () => null,
+      },
+      {
+        label: 'Types',
+        value: 'TYPES',
+        feature: null,
+        position: 0,
+        type: 'OPTION',
+        isActive: true,
+        isBlocked: false,
+        isNew: false,
+        children: types,
+        action: () => null,
+      },
+      {
+        label: 'Participants',
+        value: 'PARTICIPANTS',
+        feature: null,
+        position: 0,
+        type: 'OPTION',
+        isActive: true,
+        isBlocked: false,
+        isNew: false,
+        children: participants,
+        action: () => null,
+      },
+    ]
   }
 
   onSortMostRecent = (ideas: Array<IdeaConfiguration>) =>
@@ -156,19 +250,6 @@ export default class History extends React.Component<
           }
           rightPartSlot={
             <div className={layouts['snackbar--tight']}>
-              <Feature
-                isActive={
-                  features.find((feature) => feature.name === 'HISTORY_FILTER')
-                    ?.isActive
-                }
-              >
-                <Dropdown
-                  id="sort-ideas"
-                  options={this.typesHandler()}
-                  selected={this.state.filteredBy}
-                  alignment="RIGHT"
-                />
-              </Feature>
               <Feature
                 isActive={
                   features.find((feature) => feature.name === 'HISTORY_SORT')
@@ -219,6 +300,22 @@ export default class History extends React.Component<
               </Feature>
               <Feature
                 isActive={
+                  features.find((feature) => feature.name === 'HISTORY_FILTER')
+                    ?.isActive
+                }
+              >
+                <Menu
+                  id="filter-ideas"
+                  type="ICON"
+                  icon="resolve-filled"
+                  options={this.typesHandler()}
+                  selected={this.state.filteredBy}
+                  isNew={this.state.filteredBy !== 'NONE'}
+                  alignment="BOTTOM_RIGHT"
+                />
+              </Feature>
+              <Feature
+                isActive={
                   features.find((feature) => feature.name === 'HISTORY_REMOVE')
                     ?.isActive
                 }
@@ -235,24 +332,39 @@ export default class History extends React.Component<
           border={['BOTTOM']}
         ></Bar>
         <div className="control__block">
-          <ul style={{
-            padding: '0 var(--size-xxsmall)'
-          }}>
+          <ul
+            style={{
+              padding: '0 var(--size-xxsmall)',
+            }}
+          >
             {this.state.ideas.map((idea, index) => (
               <SimpleItem
                 key={index}
                 leftPartSlot={
-                  <div className={`${layouts['snackbar--medium']} ${layouts['snackbar--start']}`}>
+                  <div
+                    className={`${layouts['snackbar--medium']} ${layouts['snackbar--start']}`}
+                  >
                     <div className={`${layouts['snackbar--tight']}`}>
-                      <div className="color-chip" style={{ backgroundColor: idea.type.hex }}></div>
-                      <span className={`${texts['type']} ${texts['type--secondary']} type`}>{idea.type.name}</span>
+                      <div
+                        className="color-chip"
+                        style={{ backgroundColor: idea.type.hex }}
+                      ></div>
+                      <span
+                        className={`${texts['type']} ${texts['type--secondary']} type`}
+                      >
+                        {idea.type.name}
+                      </span>
                     </div>
                     <div className={`${texts['type']} type`}>{idea.text}</div>
                   </div>
                 }
                 rightPartSlot={
                   <div className="user">
-                    <span className={`${texts['type']} ${texts['type--secondary']} type`}>{idea.userIdentity.fullName}</span>
+                    <span
+                      className={`${texts['type']} ${texts['type--secondary']} type`}
+                    >
+                      {idea.userIdentity.fullName}
+                    </span>
                     <div className="user__avatar">
                       <img
                         src={idea.userIdentity.avatar}
