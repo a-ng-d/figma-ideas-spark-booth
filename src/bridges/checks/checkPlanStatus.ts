30408fc1b6ebba04a173f904d3c48615183d8b96
Aurélien Grimaud <a.grimaud@involt.io>
2025-01-21 18:03:28 +0100

    refactor: enhance plan status check logic to include Pro feature flag

--- src/bridges/checks/checkPlanStatus.ts
+++ src/bridges/checks/checkPlanStatus.ts
@@ -1,6 +1,11 @@
-import { oldTrialTime, trialTime, trialVersion } from '../../config'
+import {
+  isProEnabled,
+  oldTrialTime,
+  trialTime,
+  trialVersion,
+} from '../../config'
 
-const checkPlanStatus = async () => {
+const checkPlanStatus = async (context = 'UI' as 'UI' | 'PARAMETERS') => {
   // figma.clientStorage.deleteAsync('trial_start_date')
   // figma.clientStorage.deleteAsync('trial_version')
   // figma.clientStorage.setAsync(
@@ -28,21 +33,26 @@ const checkPlanStatus = async () => {
     else trialStatus = 'PENDING'
   }
 
-  figma.ui.postMessage({
-    type: 'CHECK_PLAN_STATUS',
-    data: {
-      planStatus:
-        trialStatus === 'PENDING' ? 'PAID' : figma.payments?.status.type,
-      trialStatus: trialStatus,
-      trialRemainingTime: Math.ceil(
-        currentTrialVersion !== trialVersion
-          ? oldTrialTime - consumedTime
-          : trialTime - consumedTime
-      ),
-    },
-  })
+  if (context === 'UI')
+    figma.ui.postMessage({
+      type: 'CHECK_PLAN_STATUS',
+      data: {
+        planStatus:
+          trialStatus === 'PENDING' || !isProEnabled
+            ? 'PAID'
+            : figma.payments?.status.type,
+        trialStatus: trialStatus,
+        trialRemainingTime: Math.ceil(
+          currentTrialVersion !== trialVersion
+            ? oldTrialTime - consumedTime
+            : trialTime - consumedTime
+        ),
+      },
+    })
 
-  return true
+  return trialStatus === 'PENDING' || !isProEnabled
+    ? 'PAID'
+    : figma.payments?.status.type
 }
 
 export default checkPlanStatus
