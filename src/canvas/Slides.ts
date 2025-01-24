f854212168b5ee316060e902f5666f3653963f67
Aurélien Grimaud <a.grimaud@involt.io>
2025-01-24 21:59:18 +0100

    feat: enhance slide creation with improved layout and styling for session date and participants list

--- src/canvas/Slides.ts
+++ src/canvas/Slides.ts
@@ -76,12 +76,96 @@ export default class Slides {
   ) => {
     // Base
     const slideNode = figma.createSlide()
-    slideNode.name = `${this.activityName}・${name}・${setFriendlyDate(this.sessionDate, lang)}`
-    slideNode.fills = [this.solidPaint(hex + '50')]
+    slideNode.name = `${this.activityName}・${setFriendlyDate(this.sessionDate, lang)}・${name}`
+    slideNode.layoutGrids = [
+      {
+        pattern: 'ROWS',
+        count: 1,
+        gutterSize: Slides.slideGaps.vertical,
+        offset: Slides.slideGaps.horizontal,
+        alignment: 'STRETCH',
+        visible: false,
+        color: { r: 1, g: 0, b: 0, a: 0.1 },
+      },
+      {
+        pattern: 'COLUMNS',
+        count: 1,
+        gutterSize: Slides.slideGaps.vertical,
+        offset: Slides.slideGaps.horizontal,
+        alignment: 'STRETCH',
+        visible: false,
+        color: { r: 1, g: 0, b: 0, a: 0.1 },
+      },
+    ]
+    slideNode.fills = [this.solidPaint(hex + '20')]
 
-    // Sticky Notes
+    // Layout
+    const layoutNode = figma.createFrame()
+    slideNode.appendChild(layoutNode)
+    layoutNode.name = '_layout'
+    layoutNode.layoutMode = 'VERTICAL'
+    layoutNode.primaryAxisSizingMode = 'FIXED'
+    layoutNode.counterAxisSizingMode = 'FIXED'
+    layoutNode.itemSpacing = 72
+    layoutNode.verticalPadding = Slides.slideGaps.vertical
+    layoutNode.horizontalPadding = Slides.slideGaps.horizontal
+    layoutNode.resize(Slides.slideWidth, Slides.slideHeight)
+    layoutNode.fills = []
+
+    // Header
+    const headerNode = figma.createFrame()
+    layoutNode.appendChild(headerNode)
+    headerNode.name = '_header'
+    headerNode.layoutMode = 'VERTICAL'
+    headerNode.layoutSizingHorizontal = 'FILL'
+    headerNode.layoutSizingVertical = 'HUG'
+    headerNode.itemSpacing = gaps.small
+    headerNode.fills = []
+
+    // Session Date
+    const sessionNode = figma.createText()
+    headerNode.appendChild(sessionNode)
+    sessionNode.name = '_session-date'
+    sessionNode.layoutSizingHorizontal = 'FILL'
+    sessionNode.layoutSizingVertical = 'HUG'
+    sessionNode.characters = setFriendlyDate(this.sessionDate, lang, 'LONG')
+    sessionNode.textAutoResize = 'WIDTH_AND_HEIGHT'
+    sessionNode.fontSize = textStyles.slideSubTitle.fontSize
+    sessionNode.fontName = {
+      family: textStyles.slideSubTitle.fontFamily,
+      style: textStyles.slideSubTitle.fontWeight,
+    }
+    sessionNode.fills = [this.solidPaint(colors.lightColor)]
+
+    // Tyme
+    const typeNode = figma.createText()
+    headerNode.appendChild(typeNode)
+    typeNode.name = '_type-name'
+    typeNode.layoutSizingHorizontal = 'FILL'
+    typeNode.layoutSizingVertical = 'HUG'
+    typeNode.characters = name
+    typeNode.textAutoResize = 'WIDTH_AND_HEIGHT'
+    typeNode.fontSize = textStyles.slideTitle.fontSize
+    typeNode.fontName = {
+      family: textStyles.slideTitle.fontFamily,
+      style: textStyles.slideTitle.fontWeight,
+    }
+    typeNode.fills = [this.solidPaint(colors.lightColor)]
+
+    // Ideas
+    const ideasNode = figma.createFrame()
+    layoutNode.appendChild(ideasNode)
+    ideasNode.name = '_ideas-list'
+    ideasNode.layoutMode = 'HORIZONTAL'
+    ideasNode.layoutSizingHorizontal = 'FILL'
+    ideasNode.layoutSizingVertical = 'FILL'
+    ideasNode.layoutWrap = 'WRAP'
+    ideasNode.itemSpacing = gaps.large
+    ideasNode.counterAxisSpacing = null
+    ideasNode.fills = []
+
     const stickyNotes = ideas.map((idea) => this.makeStickyNote(idea, hex))
-    stickyNotes.flat().forEach((note) => slideNode.appendChild(note))
+    stickyNotes.flat().forEach((note) => ideasNode.appendChild(note))
 
     return slideNode
   }
@@ -223,7 +307,7 @@ export default class Slides {
     // Participants List
     const participantsListNode = figma.createFrame()
     participantsNode.appendChild(participantsListNode)
-    participantsListNode.name = '_list'
+    participantsListNode.name = '_participants-list'
     participantsListNode.layoutMode = 'HORIZONTAL'
     participantsListNode.layoutSizingHorizontal = 'FILL'
     participantsListNode.layoutSizingVertical = 'HUG'
