079e53fce4c14225669a1b90c396c45efa77c9e3
Aurélien Grimaud <a.grimaud@involt.io>
2025-03-05 14:23:19 +0100

    fix: simplify highlight status check by removing unnecessary conditions

--- src/bridges/checks/checkHighlightStatus.ts
+++ src/bridges/checks/checkHighlightStatus.ts
@@ -1,6 +1,5 @@
 const checkHighlightStatus = async (remoteVersion: string) => {
   const localVersion = await figma.clientStorage.getAsync('highlight_version')
-
   if (localVersion === undefined && remoteVersion === undefined)
     return figma.ui.postMessage({
       type: 'PUSH_HIGHLIGHT_STATUS',
@@ -9,12 +8,7 @@ const checkHighlightStatus = async (remoteVersion: string) => {
   else if (localVersion === undefined)
     return figma.ui.postMessage({
       type: 'PUSH_HIGHLIGHT_STATUS',
-      data:
-        figma.payments !== undefined
-          ? figma.payments.getUserFirstRanSecondsAgo() > 60
-            ? 'DISPLAY_HIGHLIGHT_DIALOG'
-            : 'NO_HIGHLIGHT'
-          : 'NO_HIGHLIGHT',
+      data: 'NO_HIGHLIGHT',
     })
   else {
     const remoteMajorVersion = remoteVersion.split('.')[0],
